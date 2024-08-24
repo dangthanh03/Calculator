9daa4c2e192a8b6a8b64f0dc98ddf5396874cd79
FIXED-TERM Dang Tri Thanh (MS/EMN53) <dtn8hc@bosch.com>
2024-08-24 13:31:07 +0700

    Completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { UserInputComponent } from './user-input/user-input.component';
+import { HeaderComponent } from './header/header.component';
+
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+@Component({
+  selector: 'app-root',
+  
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
